ebb8dd2d528a1e35e1064fa46ef9ab26abe16cc3
Ben Boyter <bboyte01@gmail.com>
2015-03-02 18:33:44 +1100

    Adding in steps to go with presentation. Step1 is displaying the histogram and the values that we want to collect. Step2 is putting this into practice and saving the image.

---
+++ code/step1.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from operator import itemgetter
+
+im = Image.open("captcha.gif")
+im = im.convert("P")
+his = im.histogram()
+
+values = {}
+
+for i in range(256):
+  values[i] = his[i]
+
+print "\r\nHistorgram Values"
+print his
+
+
+print "\r\nSorted Colours"
+for j, k in sorted(values.items(), key=itemgetter(1), reverse=True)[:10]:
+  print j, k
